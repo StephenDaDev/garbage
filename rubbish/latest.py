16a784ca00cfb3aae2a97737ecec4272fbc243dd
DAzVise <52792999+DAzVise@users.noreply.github.com>
2019-08-23 23:48:04 +0300

    Update latest.py

--- rubbish/latest.py
+++ rubbish/latest.py
@@ -1,12 +1,5 @@
-import asyncio
 import discord
 from discord.ext import commands
-
-from core import checks
-from core.models import PermissionLevel
-
-Cog = getattr(commands, "Cog", object)
-
 
 class latest(Cog):
     """Find out whats the latest plugin to come to the market!
@@ -14,10 +7,9 @@ class latest(Cog):
 
 
     @commands.command(aliases=["newest"])
-    @checks.has_permissions(PermissionLevel.REGULAR)
     async def latest(self, ctx):
         """Send the latest plugin to be added to the registry"""
-          await bot.say("The latest plugin is DAzVise's ServerStats Plugin!")
+          await ctx.send("The latest plugin is DAzVise's ServerStats Plugin!")
 
 
 def setup(bot):
