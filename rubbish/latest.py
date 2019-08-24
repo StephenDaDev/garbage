e825f2897cc3cb8d68a25b7546d4a7d719940f9e
Stephen <48072084+StephenDaDev@users.noreply.github.com>
2019-08-23 20:17:50 -0400

    Update latest.py

--- rubbish/latest.py
+++ rubbish/latest.py
@@ -10,7 +10,7 @@ class latest(commands.Cog):
     @commands.command(aliases=["newest"])
     async def latest(self, ctx):
         """Send the latest plugin to be added to the registry"""
-          await ctx.send("The latest plugin is DAzVise's ServerStats Plugin!")
+          await ctx.send("This doesn't work because they would need to update for it to work...")
 
 
 def setup(bot):
