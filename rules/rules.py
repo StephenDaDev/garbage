6b9b2aae7065baf18a35b2e059b5bb7d2f437c34
Stephen <48072084+StephenDaDev@users.noreply.github.com>
2019-11-16 10:45:20 -0500

    Update rules.py

--- rules/rules.py
+++ rules/rules.py
@@ -18,7 +18,7 @@ class sRules(Cog):
         """Send preconfigured rules - loose."""
         embed = discord.Embed(
             title="Server Rules"
-            description="> ``1.`` No Spamming.\nThis will result in a kick.\n\n > ``2.`` No Trolling. This will result in an instant ban.\n\n > ``3.`` Do NOT mass ping any group of users or roles. Pinging a role counts as mass pinging.\n\n > ``4.`` Respect All Members. Staff and Members, everyone deserves respect.\n\n > ``5.`` Any form of NSFW content is prohibited in any and all channels within this server. A ban will be coming your way for violating this.\n\n > ``6.`` You are REQUIRED to follow Discord Terms of Service [Discord TOS](https://www.discordapp.com/tos) > ``7.`` You are REQUIRED to follow Discord's Community [Guidelines](https://www.discordapp.com/guidelines)",
+            description="> ``1.`` No Spamming.\nThis will result in a kick.\n\n > ``2.`` No Trolling. This will result in an instant ban.\n\n > ``3.`` Do NOT mass ping any group of users or roles. Pinging a role counts as mass pinging.\n\n > ``4.`` Respect All Members. Staff and Members, everyone deserves respect.\n\n > ``5.`` Any form of NSFW content is prohibited in any and all channels within this server. A ban will be coming your way for violating this.\n\n > ``6.`` You are REQUIRED to follow Discord Terms of Service [Discord TOS] \n\n > ``7.`` You are REQUIRED to follow Discord's Community [Guidelines](https://www.discordapp.com/guidelines)",
             color=self.bot.main_color,
         )
         return await ctx.send(embed=embed)
