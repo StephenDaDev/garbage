16cc4faa20b1ce09fc322fcd5e9641352459f56b
Stephen <48072084+StephenDaDev@users.noreply.github.com>
2019-11-16 23:53:04 -0500

    piyush said this may work

--- rules/rules.py
+++ rules/rules.py
@@ -18,7 +18,9 @@ class sRules(Cog):
         """Send preconfigured rules - loose."""
         embed = discord.Embed(
             title="Server Rules"
-            description="""
+            color=self.bot.main_color,
+        )
+        embed.description="""
                 > ``1.`` No Spamming. 
                 • This will result in a kick.
                 > ``2.`` No Trolling.
@@ -34,8 +36,6 @@ class sRules(Cog):
                 > ``7.`` You are REQUIRED to follow Discord's Community Guidelines.
             • https://www.discordapp.com/guidelines
             """,
-            color=self.bot.main_color,
-        )
         return await ctx.send(embed=embed)
 
 
